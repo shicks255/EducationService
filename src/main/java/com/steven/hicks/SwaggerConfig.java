3c9125ddec63813cc9ed67a09529e525b46f586c
shicks <shicks255@yahoo.com>
2019-08-02 19:17:16 -0500

    more swagger configuration

--- src/main/java/com/steven/hicks/SwaggerConfig.java
+++ src/main/java/com/steven/hicks/SwaggerConfig.java
@@ -4,9 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 
 // /v2/api-docs
@@ -23,6 +27,12 @@ public class SwaggerConfig
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo()
+    {
+        return new ApiInfo("Steve's Education Service", "Education info", "2.0", "", new Contact("steve hicks", "", ""), "", "", Collections.emptyList());
     }
 }
--- src/main/java/com/steven/hicks/controllers/SchoolController.java
+++ src/main/java/com/steven/hicks/controllers/SchoolController.java
@@ -2,13 +2,16 @@ package com.steven.hicks.controllers;
 
 import com.steven.hicks.entities.School;
 import com.steven.hicks.repositories.SchoolService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/school")
+@RequestMapping("/schools")
+@Api(value = "schools")
 public class SchoolController
 {
     private SchoolService m_schoolService;
@@ -20,6 +23,7 @@ public class SchoolController
     }
 
     @GetMapping("/{acronim}")
+    @ApiOperation(value = "Get school by school Acronim", notes = "acronyms are case sensitive")
     public School getSchool(@PathVariable String acronim)
     {
         School school = m_schoolService.getByAcronim(acronim);
