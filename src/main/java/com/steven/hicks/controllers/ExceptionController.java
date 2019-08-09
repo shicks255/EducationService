9c493b404cc45e11a8227a0fe38f34fbbf4aa11c
shicks <shicks255@yahoo.com>
2019-08-08 21:28:42 -0500

    starting to add stuff for custom exception handling

---
+++ src/main/java/com/steven/hicks/controllers/ExceptionController.java
@@ -0,0 +1,9 @@
+package com.steven.hicks.controllers;
+
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+public class ExceptionController extends ResponseEntityExceptionHandler
+{
+
+
+}
